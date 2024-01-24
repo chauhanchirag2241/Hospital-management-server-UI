b0a678310e97ed1ea95ad764407053878d67594b
chirag <coderchirag2272@gmail.com>
2024-01-24 23:59:21 +0530

    add reactive forms in employee form.

--- src/app/view/Admin/doctor/doctor.component.ts
+++ src/app/view/Admin/doctor/doctor.component.ts
@@ -1,10 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
 import { NewdoctorComponent } from './newdoctor/newdoctor.component';
 
@@ -14,10 +18,14 @@ import { NewdoctorComponent } from './newdoctor/newdoctor.component';
   standalone: true,
   templateUrl: './doctor.component.html',
   styleUrls: ['./doctor.component.css'],
-  imports: [MatDialogModule,MatFormFieldModule,  MatTableModule, MatIconModule, MatFormFieldModule, MatInputModule, FormsModule]
+  imports: [CommonModule, MatDialogModule, MatFormFieldModule, MatTableModule, MatIconModule, MatFormFieldModule, MatInputModule, FormsModule, MatCardModule, MatSelectModule, MatDividerModule, ReactiveFormsModule]
 })
 export class DoctorComponent implements OnInit {
-  
+  /* variables*/
+  isShowList: boolean = true;
+  isAdd: boolean = false;
+  /*end variable*/
+
   ngOnInit() {
 
     this.dataSource = [
@@ -39,19 +47,44 @@ export class DoctorComponent implements OnInit {
   displayedColumns: string[] = ['doctorcode','doctorname', 'qualification', 'jobspecification','edit','delete'];
   dataSource: any;
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, public formBuilder: FormBuilder,) { }
 
-  openDialog() {
-    const dialogRef = this.dialog.open(NewdoctorComponent, {
-      //backdropClass: 'dialogstyle'
-      // height: '600px', width: '1500px', 
-      //data: [this.admissionById, this.isEditScreen],
-      //  admissionData : this.admissionById,
-    });
+  //openDialog() {
+  //  const dialogRef = this.dialog.open(NewdoctorComponent, {
+  //    //backdropClass: 'dialogstyle'
+  //    // height: '600px', width: '1500px', 
+  //    //data: [this.admissionById, this.isEditScreen],
+  //    //  admissionData : this.admissionById,
+  //  });
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
-    });
+  //  dialogRef.afterClosed().subscribe(result => {
+  //    console.log(`Dialog result: ${result}`);
+  //  });
+  //}
+
+
+  employeeForm = this.formBuilder.group({
+    department: ['', [Validators.required]],
+    emptype: ['', [Validators.required]],
+    empcode: ['', [Validators.required, Validators.maxLength(4), Validators.minLength(4)]],
+    empname: ['', [Validators.required, Validators.maxLength(25)]],
+    gender: ['', [Validators.required]],
+    mobileno: ['', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
+    email: ['', [Validators.required, Validators.email]],
+    qualification: ['', [Validators.required]],
+    jobspecification: ['', [Validators.required]]
+  });
+
+  addNewEmployee() {
+    console.log(this.employeeForm.value);
+  }
+  onAddBtnClick() {
+    this.isShowList = false;
+    this.isAdd = true;
   }
 
+  onBackBtnClick() {
+    this.isShowList = true;
+    this.isAdd = false;
+  }
 }
--- src/app/view/Admin/doctor/newdoctor/newdoctor.component.ts
+++ src/app/view/Admin/doctor/newdoctor/newdoctor.component.ts
@@ -5,14 +5,18 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDividerModule } from '@angular/material/divider';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { DoctorComponent } from '../doctor.component';
 
 @Component({
   selector: 'app-newdoctor',
   templateUrl: './newdoctor.component.html',
   styleUrls: ['./newdoctor.component.css'],
   standalone: true,
-  imports: [MatInputModule, MatIconModule, MatSelectModule, MatFormFieldModule, MatCardModule, MatDividerModule]
+  imports: [CommonModule, MatInputModule, MatIconModule, MatSelectModule, MatFormFieldModule, MatCardModule, MatDividerModule, ReactiveFormsModule]
 })
-export class NewdoctorComponent {
+export class NewdoctorComponent  {
+
 
 }
