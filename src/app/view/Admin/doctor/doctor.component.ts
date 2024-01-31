20acb85cf3ce4879d85c7a9b8eb0944d82e08682
chirag <coderchirag2272@gmail.com>
2024-01-31 23:33:24 +0530

    add api call

--- src/app/view/Admin/doctor/doctor.component.ts
+++ src/app/view/Admin/doctor/doctor.component.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -10,6 +11,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
+import { BrowserModule } from '@angular/platform-browser';
 import { NewdoctorComponent } from './newdoctor/newdoctor.component';
 
 
@@ -18,12 +20,14 @@ import { NewdoctorComponent } from './newdoctor/newdoctor.component';
   standalone: true,
   templateUrl: './doctor.component.html',
   styleUrls: ['./doctor.component.css'],
-  imports: [CommonModule, MatDialogModule, MatFormFieldModule, MatTableModule, MatIconModule, MatFormFieldModule, MatInputModule, FormsModule, MatCardModule, MatSelectModule, MatDividerModule, ReactiveFormsModule]
+  imports: [CommonModule, HttpClientModule, MatDialogModule, MatFormFieldModule, MatTableModule, MatIconModule, MatFormFieldModule, MatInputModule, FormsModule, MatCardModule, MatSelectModule, MatDividerModule, ReactiveFormsModule],
+  providers: [ HttpClient]
 })
 export class DoctorComponent implements OnInit {
   /* variables*/
   isShowList: boolean = true;
   isAdd: boolean = false;
+  response: any;
   /*end variable*/
 
   ngOnInit() {
@@ -47,8 +51,8 @@ export class DoctorComponent implements OnInit {
   displayedColumns: string[] = ['doctorcode','doctorname', 'qualification', 'jobspecification','edit','delete'];
   dataSource: any;
 
-  constructor(public dialog: MatDialog, public formBuilder: FormBuilder,) { }
-
+  constructor( public formBuilder: FormBuilder, private http: HttpClient) { }
+  //, private toasrt: ToastrService
   //openDialog() {
   //  const dialogRef = this.dialog.open(NewdoctorComponent, {
   //    //backdropClass: 'dialogstyle'
@@ -64,10 +68,11 @@ export class DoctorComponent implements OnInit {
 
 
   employeeForm = this.formBuilder.group({
-    department: ['', [Validators.required]],
-    emptype: ['', [Validators.required]],
-    empcode: ['', [Validators.required, Validators.maxLength(4), Validators.minLength(4)]],
-    empname: ['', [Validators.required, Validators.maxLength(25)]],
+    employeeid:[0],
+    departmentid: ['', [Validators.required]],
+    employeetype: ['', [Validators.required]],
+    employeecode: ['', [Validators.required, Validators.maxLength(4), Validators.minLength(4)]],
+    employeename: ['', [Validators.required, Validators.maxLength(25)]],
     gender: ['', [Validators.required]],
     mobileno: ['', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
     email: ['', [Validators.required, Validators.email]],
@@ -77,6 +82,17 @@ export class DoctorComponent implements OnInit {
 
   addNewEmployee() {
     console.log(this.employeeForm.value);
+    this.http.post<any>("https://localhost:7087/api/Employee/Add", this.employeeForm.value)
+      .subscribe((res) => {
+        this.response = res;
+        if (this.response.statusCode == 200) {
+          //this.toasrt.success("Employee Created. 😎")
+          //this.router.navigate(['employee']);
+        }
+        else {
+         // this.toasrt.error("Error While Creating Employee. 😒😰")
+        }
+      });
   }
   onAddBtnClick() {
     this.isShowList = false;
