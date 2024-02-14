662b27551c2a13f393e02cf835aafff038c42189
chirag <coderchirag2272@gmail.com>
2024-02-14 23:30:56 +0530

    paitent visite

--- src/app/view/Admin/paitentvisitedashboard/paitentvisitedashboard.component.ts
+++ src/app/view/Admin/paitentvisitedashboard/paitentvisitedashboard.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from '../../../Services/login.service';
 
 @Component({
   selector: 'app-paitentvisitedashboard',
@@ -6,5 +9,105 @@ import { Component } from '@angular/core';
   styleUrls: ['./paitentvisitedashboard.component.css']
 })
 export class PaitentvisitedashboardComponent {
+  /*Variable*/
+  paitentVissiteList: any;
+  loginData: any;
+  userName: any;
+  paitentForm!: FormGroup;
+  response: any;
+  medicineList: any;
+  departmentList: any;
+  doctorList: any;
+  paitentList: any;
+  /*end variable*/
+  constructor(public formBuilder: FormBuilder, private http: HttpClient, private login: LoginService) { }
+
+  ngOnInit(): void {
+    this.login.shareLoginData.subscribe((x: any) => this.loginData = x);
+    console.log("from paitentvisite : " + this.loginData);
+    this.userName = this.loginData[0].employeeName;
+    this.getPaitentVisiteByEmployeeId();
+    this.createForm();
+    this.getDepartment();
+    this.getAllMedicine();
+  }
+
+  getPaitentVisiteByEmployeeId() {
+    const emplooyeeId = this.loginData[0].employeeId;
+    this.http.get<any>(`https://localhost:7087/api/PaitentVisite/GetAllAssignPaitent/${emplooyeeId}`).subscribe((res) => {
+      this.paitentVissiteList = res[0];
+      console.log(this.paitentVissiteList);
+      this.paitentForm.patchValue({
+        description: this.paitentVissiteList.description,
+        paitentId: this.paitentVissiteList.paitentId,
+     
+      });
+      this.getPaitent();
+    })  
+  }
+  getPaitent() {
+    const paitentId = this.paitentVissiteList.paitentId;
+      this.http.get<any>(`https://localhost:7087/api/Paitent/GetPaitentByPaitentId/${paitentId}`)
+        .subscribe((res) => {
+          this.paitentList = res[0];
+          this.paitentForm.patchValue({
+            paitentName: this.paitentList.firstName + " " + this.paitentList.lastName,
+            paitentId: this.paitentList.paitentId,
+            medicalIssue: this.paitentList.medicalIssue,
+          });
+        });
+
+  }
+  createForm() {
+    this.paitentForm = this.formBuilder.group({
+      paitentId: [''],
+      paitentName: ['', [Validators.required]],
+      medicalIssue: ['', [Validators.required]],
+      assignToId: ['', [Validators.required]],
+      addedById: [''],
+      status: [''],
+      description: [''],
+      medicineIds: [''],
+
+    });
+  }
+
+  addPaitentVisite() {
+    console.log(this.paitentForm.value);
+    this.paitentForm.patchValue({
+      addedById: this.loginData[0].employeeId,
+    });
+    this.http.post<any>("https://localhost:7087/api/PaitentVisite/Add", this.paitentForm.value)
+      .subscribe((res) => {
+        this.response = res;
+      });
+  }
+
+
+  getAllMedicine() {
+
+    this.http.get<any>(`https://localhost:7087/api/medicine/GetAllMedicine`)
+      .subscribe((res) => {
+        this.medicineList = res[0];
+      });
+
+  }
+
+  getDepartment() {
+    this.http.get<any>("https://localhost:7087/api/Department/GetAllDepartment").subscribe((response) => {
+      this.departmentList = response;
+    })
+  }
+  getDoctors(depId: any) {
+
+    this.http.get<any>(`https://localhost:7087/api/employee/GetDoctor/${depId}`)
+      .subscribe((res) => {
+        this.doctorList = res;
+      });
+
+    //this.http.get<any>("https://localhost:7087/api/employee/GetDoctor").subscribe((response) => {
+    //  this.doctorList = response;
+    //})
+  }
 
 }
